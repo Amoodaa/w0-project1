871a44be83e92c4e2543c6d880118a5259941543
Imad Shatali <Imad.shatali@gmail.com>
2019-06-16 16:22:09 +0300

    Finished week0 day1 project

--- js/index.js
+++ js/index.js
@@ -14,28 +14,86 @@ const siteContent = {
     "img-src": "img/header-img.png"
   },
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
   "footer": {
-    "copyright" : "Copyright Great Idea! 2019"
+    "copyright": "Copyright Great Idea! 2019"
   },
 };
 
 // write your code here 
 
+// nav
+
+const navArr = Array.from(document.querySelectorAll('nav > a'));
+let navTexts = Object.values(siteContent.nav);
+navArr.forEach((a, i) => {
+  a.style.color = 'green';
+  a.textContent = navTexts[i];
+});
+const a1 = document.createElement('a');
+const a2 = document.createElement('a');
+a1.textContent = 'Creativity';
+a2.textContent = 'Inspiration';
+a1.style.color = 'green';
+a2.style.color = 'green';
+document.querySelector('nav').appendChild(a1);
+document.querySelector('nav').appendChild(a2);
+//logo
+document.querySelector('#logo-img').src = siteContent.nav["img-src"];
+
+
+// section cta
+const ctaH1 = document.querySelector('.cta-text > h1');
+ctaH1.textContent = siteContent.cta.h1;
+
+const ctaBtn = document.querySelector('.cta-text > button');
+ctaBtn.textContent = siteContent.cta.button;
+
+document.querySelector('#cta-img').src = siteContent.cta["img-src"];
+
+
+//section main-content
+const mainContent =
+  Array.from(document.querySelectorAll('.text-content')).map(e => {
+    return Object.values(e.children);
+  }).flat(1);
+
+let mainContentArr = Object.assign(siteContent["main-content"]); //preparing the obj for looping
+document.querySelector('#middle-img').src = mainContentArr["middle-img-src"]; // doing the img first
+delete mainContentArr['middle-img-src'];
+mainContentArr = Object.values(mainContentArr);
+
+mainContent.forEach((e, i) => {
+  e.textContent = mainContentArr[i];
+});
+
+
+// section contact
+const contactArr = Object.values(siteContent.contact);
+const contactElements = Array.from(document.querySelectorAll('.contact')).map(e => {
+  return Object.values(e.children);
+}).flat(1);
+contactElements.forEach((e, i) => {
+  e.textContent = contactArr[i];
+});
+
+
+// footer
+document.querySelector('footer').textContent = siteContent.footer.copyright;
---
+++ js/indexStretched.js
@@ -0,0 +1,226 @@
+const siteContentObj = [{
+    "nav": {
+      "nav-item-1": "Services",
+      "nav-item-2": "Product",
+      "nav-item-3": "Vision",
+      "nav-item-4": "Features",
+      "nav-item-5": "About",
+      "nav-item-6": "Contact",
+      "img-src": "img/logo.png"
+    },
+    "cta": {
+      "h1": "DOM Is Awesome",
+      "button": "Get Started",
+      "img-src": "img/header-img.png"
+    },
+    "main-content": {
+      "features-h4": "Features",
+      "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+      "about-h4": "About",
+      "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+      "middle-img-src": "img/mid-page.jpg",
+      "services-h4": "Services",
+      "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+      "product-h4": "Product",
+      "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+      "vision-h4": "Vision",
+      "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    },
+    "contact": {
+      "contact-h4": "Contact",
+      "address": "123 Way 456 Street Somewhere, USA",
+      "phone": "1 (888) 888-8888",
+      "email": "sales@greatidea.io",
+    },
+    "footer": {
+      "copyright": "Copyright Great Idea! 2019"
+    },
+  },
+
+  {
+    "nav": {
+      "nav-item-1": "Services",
+      "nav-item-2": "Product",
+      "nav-item-3": "Vision",
+      "nav-item-4": "Features",
+      "nav-item-5": "About",
+      "nav-item-6": "Contact",
+      "img-src": "img/logo.png"
+    },
+    "cta": {
+      "h1": "quis culpa esse",
+      "button": "Get Started",
+      "img-src": "img/header-img.png"
+    },
+    "main-content": {
+      "features-h4": "Features",
+      "features-content": "Nulla dolore adipisicing id pariatur voluptate sunt aliqua tempor. Minim eiusmod eu eiusmod elit. Mollit officia est ad exercitation excepteur in ullamco cupidatat excepteur. Labore dolore aliquip dolor cillum esse laboris reprehenderit est irure est excepteur consectetur. Aliqua officia laborum fugiat labore eiusmod qui est. Esse ea aute ut duis consectetur ullamco eiusmod dolore do culpa excepteur.",
+      "about-h4": "About",
+      "about-content": "Adipisicing ullamco ad ea voluptate magna adipisicing aliqua reprehenderit aute velit Lorem anim tempor. Aliqua tempor fugiat nisi exercitation nostrud labore proident qui ut Lorem nostrud. Ullamco cillum qui ut sunt duis reprehenderit duis ullamco pariatur cillum irure quis duis. Enim ea elit enim pariatur proident ex ipsum Lorem mollit veniam ad dolor Lorem occaecat. Proident sit exercitation pariatur elit adipisicing anim exercitation exercitation fugiat.",
+      "middle-img-src": "img/mid-page.jpg",
+      "services-h4": "Services",
+      "services-content": "Dolore voluptate minim veniam esse est eu nulla. Id cupidatat id ad irure ullamco amet occaecat duis. Ex proident magna aliqua qui est ullamco Lorem est amet et voluptate excepteur. Nulla amet id sint eu voluptate reprehenderit cupidatat voluptate reprehenderit incididunt.",
+      "product-h4": "Product",
+      "product-content": "Excepteur commodo mollit eiusmod eu duis occaecat Lorem. Consequat elit mollit labore Lorem commodo esse enim laboris sint sunt dolore irure nisi. Cillum enim Lorem nulla labore aliqua Lorem ut eiusmod esse incididunt est. Anim ad ipsum ut amet esse aliqua esse duis adipisicing officia. Pariatur aute cupidatat laborum elit minim consequat consequat occaecat cillum. Nostrud dolore id et nulla eiusmod sit nostrud nostrud. Duis esse ea consectetur elit ex cupidatat do eiusmod fugiat pariatur non aute.",
+      "vision-h4": "Vision",
+      "vision-content": "Ea enim aliquip ipsum aliquip ut. Eu ex eu cillum nostrud do. Aliqua incididunt et incididunt consequat. Est mollit fugiat nulla et sint reprehenderit minim laborum aliqua veniam incididunt. Veniam elit laborum officia voluptate nulla eu irure magna non tempor quis sit. Non commodo sit adipisicing reprehenderit adipisicing excepteur. Ipsum non velit quis do eiusmod excepteur tempor pariatur."
+    },
+    "contact": {
+      "contact-h4": "Contact",
+      "address": "Bay Avenue",
+      "phone": "(825) 437-3102",
+      "email": "sales@greatidea.io"
+    },
+    "footer": {
+      "copyright": "Copyright Great Idea! 2019"
+    }
+  }, {
+    "nav": {
+      "nav-item-1": "Services",
+      "nav-item-2": "Product",
+      "nav-item-3": "Vision",
+      "nav-item-4": "Features",
+      "nav-item-5": "About",
+      "nav-item-6": "Contact",
+      "img-src": "img/logo.png"
+    },
+    "cta": {
+      "h1": "ut reprehenderit cupidatat",
+      "button": "Get Started",
+      "img-src": "img/header-img.png"
+    },
+    "main-content": {
+      "features-h4": "Features",
+      "features-content": "Ea ea eiusmod proident nulla non consequat velit velit laborum non quis anim. Enim aliquip cillum irure consectetur enim reprehenderit magna Lorem aliqua cupidatat irure reprehenderit consequat anim. Culpa nulla non nulla proident ea elit nulla veniam esse irure in eiusmod fugiat dolor. Minim exercitation esse et qui excepteur est. Sunt aliqua quis do cillum anim id commodo nisi culpa reprehenderit exercitation ipsum.",
+      "about-h4": "About",
+      "about-content": "Sint deserunt sit occaecat veniam ea nostrud deserunt cillum aliqua eu veniam. Ullamco tempor dolore enim culpa quis dolor Lorem in sint in. Elit consequat ut ullamco in tempor officia ullamco irure mollit aliqua amet ea pariatur excepteur. Ad reprehenderit quis occaecat excepteur in magna. Nulla cupidatat cillum culpa duis irure tempor ad quis tempor anim commodo laboris enim. Aliqua dolore esse do aliquip do eu aliqua commodo ullamco tempor.",
+      "middle-img-src": "img/mid-page.jpg",
+      "services-h4": "Services",
+      "services-content": "Ullamco deserunt ipsum excepteur esse laborum. In sit quis consequat ea et. Aliquip commodo nostrud eiusmod ea. Velit tempor quis excepteur aliqua aliquip enim velit nulla consequat elit tempor. Dolor voluptate eu officia sint duis sunt ullamco ex aliquip duis. Pariatur cillum velit qui sit nisi commodo. Velit esse est ullamco proident qui ut incididunt.",
+      "product-h4": "Product",
+      "product-content": "Qui mollit deserunt irure ex veniam ex aliquip. Laborum et ut commodo exercitation. Consectetur tempor sit ea ipsum dolore aliqua mollit sint aute reprehenderit. Id incididunt ut laboris enim magna cupidatat. Reprehenderit nulla ea sint ullamco veniam consectetur. Sunt ipsum Lorem fugiat excepteur sit.",
+      "vision-h4": "Vision",
+      "vision-content": "Commodo minim ex reprehenderit excepteur quis ad commodo. Irure reprehenderit ex minim adipisicing Lorem laboris laboris exercitation pariatur adipisicing proident et tempor. Amet tempor voluptate deserunt magna minim nisi fugiat tempor adipisicing culpa voluptate labore."
+    },
+    "contact": {
+      "contact-h4": "Contact",
+      "address": "Dunne Court",
+      "phone": "(883) 567-3804",
+      "email": "sales@greatidea.io"
+    },
+    "footer": {
+      "copyright": "Copyright Great Idea! 2019"
+    }
+  }, {
+    "nav": {
+      "nav-item-1": "Services",
+      "nav-item-2": "Product",
+      "nav-item-3": "Vision",
+      "nav-item-4": "Features",
+      "nav-item-5": "About",
+      "nav-item-6": "Contact",
+      "img-src": "img/logo.png"
+    },
+    "cta": {
+      "h1": "incididunt proident ex",
+      "button": "Get Started",
+      "img-src": "img/header-img.png"
+    },
+    "main-content": {
+      "features-h4": "Features",
+      "features-content": "Lorem commodo mollit in anim proident nisi in tempor irure do ad eiusmod. Dolore veniam duis sit adipisicing cillum in ut nulla esse exercitation occaecat. Do occaecat nulla ut ullamco. Dolor ad duis aute mollit occaecat. Voluptate qui reprehenderit reprehenderit reprehenderit occaecat magna. Adipisicing commodo id consequat ad ut velit labore.",
+      "about-h4": "About",
+      "about-content": "In mollit do do proident cupidatat quis officia Lorem veniam. Non veniam aliquip magna adipisicing consequat mollit consectetur cillum aute et non excepteur eiusmod laborum. Culpa adipisicing dolor velit excepteur mollit ex enim sit magna do nulla. Ex id dolor duis in cupidatat irure. Non ea tempor et proident labore. Sint dolore laboris reprehenderit officia cupidatat adipisicing est nulla incididunt quis irure mollit.",
+      "middle-img-src": "img/mid-page.jpg",
+      "services-h4": "Services",
+      "services-content": "Dolore ex mollit proident dolore. Nostrud consectetur consequat laborum nostrud. Magna reprehenderit eiusmod ullamco sunt dolor irure tempor id qui excepteur pariatur est. Consequat quis proident reprehenderit laboris enim exercitation occaecat nulla consequat.",
+      "product-h4": "Product",
+      "product-content": "Voluptate et eiusmod consectetur pariatur. Incididunt id officia do tempor aute officia veniam dolore. Incididunt adipisicing Lorem mollit sint mollit magna reprehenderit sunt velit nulla Lorem aute ipsum. Ad do mollit commodo adipisicing quis ipsum sint sunt velit dolor non sit non ad. Pariatur et Lorem irure nisi tempor exercitation reprehenderit tempor anim aliquip enim laborum. Voluptate magna occaecat adipisicing sit duis reprehenderit aliqua voluptate tempor ut occaecat minim.",
+      "vision-h4": "Vision",
+      "vision-content": "Minim eiusmod sint sunt in sit in nostrud dolor aliquip qui consequat reprehenderit. Ullamco culpa commodo in nisi. In occaecat non ipsum fugiat velit pariatur ullamco Lorem. Nostrud et amet cupidatat magna. Esse laborum ipsum proident nulla aliquip anim qui culpa eiusmod officia."
+    },
+    "contact": {
+      "contact-h4": "Contact",
+      "address": "Dictum Court",
+      "phone": "(974) 516-3451",
+      "email": "sales@greatidea.io"
+    },
+    "footer": {
+      "copyright": "Copyright Great Idea! 2019"
+    }
+  }
+];
+
+// write your code here
+
+const a1 = document.createElement('a');
+const a2 = document.createElement('a');
+a1.textContent = 'Creativity';
+a2.textContent = 'Inspiration';
+a1.style.color = 'green';
+a2.style.color = 'green';
+
+function changeEverything(siteContent) {
+  // nav
+  const navArr = Array.from(document.querySelectorAll('nav > a'));
+  let navTexts = Object.values(siteContent.nav);
+  navArr.forEach((a, i) => {
+    if (i < 6) {
+      a.style.color = 'green';
+      a.textContent = navTexts[i];
+    }
+  });
+
+  document.querySelector('nav').appendChild(a1);
+  document.querySelector('nav').appendChild(a2);
+  //logo
+  document.querySelector('#logo-img').src = siteContent.nav["img-src"];
+
+
+  // section cta
+  const ctaH1 = document.querySelector('.cta-text > h1');
+  ctaH1.textContent = siteContent.cta.h1;
+
+  const ctaBtn = document.querySelector('.cta-text > button');
+  ctaBtn.textContent = siteContent.cta.button;
+
+  document.querySelector('#cta-img').src = siteContent.cta["img-src"];
+
+
+  //section main-content
+  const mainContent =
+    Array.from(document.querySelectorAll('.text-content')).map(e => {
+      return Object.values(e.children);
+    }).flat(1);
+
+  let mainContentArr = Object.assign(siteContent["main-content"]); //preparing the obj for looping
+  delete mainContentArr['middle-img-src'];
+  mainContentArr = Object.values(mainContentArr);
+
+  mainContent.forEach((e, i) => {
+    e.textContent = mainContentArr[i];
+  });
+
+
+  // section contact
+  const contactArr = Object.values(siteContent.contact);
+  const contactElements = Array.from(document.querySelectorAll('.contact')).map(e => {
+    return Object.values(e.children);
+  }).flat(1);
+  contactElements.forEach((e, i) => {
+    e.textContent = contactArr[i];
+  });
+
+
+  // footer
+  document.querySelector('footer').textContent = siteContent.footer.copyright;
+}
+
+function getRandomIntInclusive(min, max) { // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Getting_a_random_integer_between_two_values_inclusive
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+document.querySelector('button').addEventListener('click', () => {
+  changeEverything(siteContentObj[getRandomIntInclusive(0, 3)]);
+});
